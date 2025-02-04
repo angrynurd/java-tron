3dfd8bdca7f0c18216310be948754683474cbbcb
andy <xq.lu@tron.network>
2025-02-04 15:43:26 +0800

    feat(api):remove unused codes in v1

--- framework/src/main/java/org/tron/core/services/http/GetNowBlockServlet.java
+++ framework/src/main/java/org/tron/core/services/http/GetNowBlockServlet.java
@@ -1,12 +1,15 @@
 package org.tron.core.services.http;
 
+import static org.tron.core.services.http.Util.existVisible;
+import static org.tron.core.services.http.Util.getVisible;
+
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tron.core.Wallet;
-import org.tron.core.services.cacheprovider.LatestBlockProvider;
 import org.tron.protos.Protocol.Block;
 
 @Component
@@ -14,13 +17,40 @@ import org.tron.protos.Protocol.Block;
 public class GetNowBlockServlet extends RateLimiterServlet {
 
   @Autowired
-  private LatestBlockProvider blockProvider;
+  private Wallet wallet;
 
-    @Override
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
     try {
+      boolean visible = getVisible(request);
+      response(response, visible);
+    } catch (Exception e) {
+      Util.processError(e, response);
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    try {
+      PostParams params = PostParams.getPostParams(request);
+      boolean visible = getVisible(request);
+      if (!existVisible(request)) {
+        visible = params.isVisible();
+      }
+      response(response, visible);
+    } catch (Exception e) {
+      Util.processError(e, response);
+    }
+  }
+
+  private void response(HttpServletResponse response, boolean visible) throws IOException {
+    Block reply = wallet.getNowBlock();
+    if (reply != null) {
+      response.getWriter().println(JsonFormat.printToString(reply, visible));
+    } else {
+      response.getWriter().println("{}");
       boolean visible = Util.getVisible(request);
-      Block reply = blockProvider.getLatestBlock();
+      Block reply = wallet.getNowBlock();
       if (reply != null) {
         response.getWriter().println(JsonFormat.printToString(reply, visible));
       } else {
